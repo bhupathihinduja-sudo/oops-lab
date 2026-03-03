0b480da6802c12a2317545a442ece9ee607c0159
bhupathihinduja-sudo <bhupathihinduja@gmail.com>
2026-03-03 22:42:01 +0530

    Add files via upload

---
+++ Datatypes and Operations/Datatypes.java
@@ -0,0 +1,11 @@
+public class Datatypes{
+public static void main(String[]args){
+int books=5;
+float costperbook=2.5f;
+float totalcost=books*costperbook;
+char currency='$';
+System.out.println("Number of books:" +books);
+System.out.println("Cost per book:" +costperbook +currency);
+System.out.println("total Cost:" +totalcost +currency);
+}
+}
