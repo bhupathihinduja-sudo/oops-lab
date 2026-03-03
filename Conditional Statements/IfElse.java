b9a2d38e69dcd581cef8b2f4163c81e6632e2769
bhupathihinduja-sudo <bhupathihinduja@gmail.com>
2026-03-03 22:43:52 +0530

    Add files via upload

---
+++ Conditional Statements/IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+  public static void main(String[] args) {
+    boolean isLoggedIn = true;
+    boolean isAdmin = false;
+    int securityLevel = 3; // 1 = highest
+
+    if (isLoggedIn && (isAdmin || securityLevel <= 2)) {
+      System.out.println("Access granted");
+    } else {
+      System.out.println("Access denied");
+    }
+}}
